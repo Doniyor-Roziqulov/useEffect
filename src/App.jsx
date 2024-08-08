f3fa65e98bd32b0e13a0bd819b476ccf559059ea
Doniyor-Roziqulov <doniyorruziqulov33@gmail.com>
2024-08-08 21:57:38 +0500

    Add file name

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Catigory from "./companents/catigoriy/Catigory";
 import Footer from "./companents/footer/Footer";
 import Goods from "./companents/goods/Goods";
 import Header from "./companents/header/Header";
-import Hero from "./companents/Hero/Hero";
+import Hero from "./companents/hero/Hero";
 import Popular from "./companents/popular/Popular";
 import Stock from "./companents/stock/Stock";
 
